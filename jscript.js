5cb4ee5d9449ec0ff0488a62ddb6b34aeb4fb5e7
Mitchell Bean <mitchtroybean@gmail.com>
2020-12-21 13:23:14 -0800

    first call

---
+++ jscript.js
@@ -0,0 +1,32 @@
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://kanjialive-api.p.rapidapi.com/api/public/search/advanced/?kem=parent",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "9df5add180mshd864f1707e5c655p1f0599jsn09d6675716fd",
+		"x-rapidapi-host": "kanjialive-api.p.rapidapi.com"
+	}
+};
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
