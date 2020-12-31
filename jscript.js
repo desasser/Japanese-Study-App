058ae16b2f545fbe3774b195524a7215c1e9959e
Daniel Sasser <desasser79@gmail.com>
2020-12-31 10:23:33 -0800

    added modals instead of alerts, working on game over screen

--- jscript.js
+++ jscript.js
@@ -16,6 +16,7 @@ $("#submit-button").on("click", function (event) {
 	$("#user-input").val('');
 })
 
+
 //Create array to store kanji searches and display them on the screen as clickable elements
 var savedKanjiArr = JSON.parse(localStorage.getItem("saved-kanji")) || [];
 
@@ -54,6 +55,12 @@ function saveKanji(savedKanji) {
 $("#search-history").on("click", "button", function () {
 	// var whichKanji = $(this).text();
 	var kanjiMeaning = $(this).data().meaning;
+
+	// do not execute a search if the clear-button is clicked
+	if (typeof kanjiMeaning === "undefined") {
+		return;
+	}
+
 	$("#media-base").empty();
 
 	//send which Kanji was clicked to the fetchApiData function
@@ -64,7 +71,8 @@ $("#search-history").on("click", "button", function () {
 var buttonEl = $('<button>');
 buttonEl.text('Clear Searches');
 buttonEl.attr('id', 'clear-button');
-$('#history').append(buttonEl);
+buttonEl.addClass('button is-dark');
+$('#search-history').append(buttonEl);
 showClearBtn();
 
 //if there are previous searches saved, show the clear button
@@ -82,6 +90,16 @@ $('#clear-button').on('click', function () {
 	localStorage.clear();
 	$('.saved-search-button').remove();
 	showClearBtn();
+})
+
+// modal controls, 'X'
+$('.close').on('click', function () {
+	$("#failed-search").css('display', 'none');
+})
+
+// modal controls, off-modal click
+$(window).on('click', function () {
+	$("#failed-search").css('display', 'none');
 })
 
 //function to fetch all data from APIs
@@ -110,8 +128,8 @@ function fetchApiData(queryTerm) {
 		console.log(response);
 
 		if (response.length === 0) {
-			//TODO: make this not an alert
-			alert('kanji not found, due to the scope of the api, some kanji are not included, please try a different word');
+			// display failed search modal
+			$("#failed-search").css('display', 'block');
 		} else {
 
 
@@ -194,8 +212,8 @@ function fetchApiData(queryTerm) {
 		method: "GET"
 	}).then(function (picture) {
 		if (picture.length === 0) {
-			//TODO: make this not an alert and say come back later, youve fetched you're alotted amount of photos, glad you are enjoying the app! no more photos will appear, but you can still enjoy the rest of the app and the kanji
-			alert('too many calls')
+			// display failed search modal
+			$("#failed-search-pic").css('display', 'block');
 		} else {
 			//new image element to house the returned image
 			var newImage = $("<img>")
@@ -207,211 +225,3 @@ function fetchApiData(queryTerm) {
 		};
 	});
 };
-
-//=====================================================================
-//=====================================================================
-//=====================================================================
-//=====================================================================
-//GAME MODE - TODO: split this into a new script page
-
-// Array of objects of kanji with their meaning and 3 incorrect answers and the correct answered marked
-var kanjiGameObject = [{
-	kanji: '蛍',
-	answers: ['mountain', 'spirit', 'orange', 'firefly'],
-	correctAnswer: 'firefly'
-},
-{
-	kanji: '蜜',
-	answers: ['fish', 'honey', 'train', 'sky'],
-	correctAnswer: 'honey'
-},
-{
-	kanji: '山',
-	answers: ['mountain', 'heart', 'ocean', 'forest'],
-	correctAnswer: 'mountain'
-},
-{
-	kanji: '気',
-	answers: ['ogre', 'plain', 'spirit', 'coffee'],
-	correctAnswer: 'spirit'
-},
-{
-	kanji: '魚',
-	answers: ['water', 'fish', 'speak', 'jacket'],
-	correctAnswer: 'fish'
-},
-{
-	kanji: '心',
-	answers: ['computer', 'desk', 'heart', 'bag'],
-	correctAnswer: 'heart'
-},
-{
-	kanji: '店',
-	answers: ['shop', 'marker', 'game', 'shoes'],
-	correctAnswer: 'shop'
-},
-{
-	kanji: '家',
-	answers: ['pants', 'restaurant', 'book', 'house'],
-	correctAnswer: 'house'
-},
-{
-	kanji: '猿',
-	answers: ['sauce', 'monkey', 'menu', 'festival'],
-	correctAnswer: 'monkey'
-},
-{
-	kanji: '雪',
-	answers: ['snow', 'summer', 'light', 'window'],
-	correctAnswer: 'snow'
-},
-{
-	kanji: '雨',
-	answers: ['rain', 'heat', 'animal', 'doctor'],
-	correctAnswer: 'rain'
-},
-{
-	kanji: '夏',
-	answers: ['merchant', 'summer', 'luck', 'dragon'],
-	correctAnswer: 'summer'
-},
-{
-	kanji: '舟',
-	answers: ['tree', 'car', 'office', 'boat'],
-	correctAnswer: 'boat'
-},
-{
-	kanji: '言',
-	answers: ['word', 'drink', 'table', 'notebook'],
-	correctAnswer: 'word'
-},
-{
-	kanji: '本',
-	answers: ['taxi', 'cat', 'food'],
-	correctAnswer: 'book'
-},
-]
-
-//TODO: BONUS: On loadup run a function to fetch 25 random kanji and fetch 75 random words, this is to eliminate downtime (while the user is familiarizing themselves with the page, JS can run in the background to generate this array and begin the quiz/game portion, more efficient)
-
-// initialize variables for global scope
-var randomKanji;
-var score = 0;
-var wrongAnswer = 0;
-var correctAnswer = 0;
-var answerP = $("<p>")
-$("#answer-hr").css("visibility", "hidden");
-$("#answers-base").css("visibility", "hidden");
-
-// function to randomly produce a question from the array
-//TODO: Store randomKanji index, check each new random num against the old list and if its there already, pick a new one
-function randomKanjiGame() {
-	// random number to select a question set from the game object
-	randomKanji = Math.floor(Math.random() * kanjiGameObject.length);
-
-	//TODO: BONUS: Create an array containing the list of answers
-	//TODO: BONUS: Shuffle that array, using https://javascript.info/task/shuffle
-	//TODO: BONUS: Then assign the values to each index as needed
-
-	// display the kanji on the page
-	$('#kanji-game-base').children().text(kanjiGameObject[randomKanji].kanji);
-
-	// set the answers into the answers cells
-	$('#option1').text(kanjiGameObject[randomKanji].answers[0]);
-	$('#option2').text(kanjiGameObject[randomKanji].answers[1]);
-	$('#option3').text(kanjiGameObject[randomKanji].answers[2]);
-	$('#option4').text(kanjiGameObject[randomKanji].correctAnswer);
-};
-
-// click event listener for start game button
-$("#start-game").on("click", function () {
-	score = 0;
-	randomKanjiGame();
-	$("#answers-base").css("visibility", "visible");
-
-	// remove start game button when clicked
-	$("#start-game").remove();
-});
-
-// click event on the list of answers, check 'this' button against the meaning from the object
-$("#answers").on("click", "button", function () {
-	console.log($(this).text());
-	console.log(randomKanji);
-	console.log(kanjiGameObject[randomKanji].correctAnswer);
-
-	// check to see if button clicked is correct and track the score, number of right answers, and number of wrong anwers
-	// display the score
-	if ($(this).text() == kanjiGameObject[randomKanji].correctAnswer) {
-		score++;
-		correctAnswer++;
-
-		answerP.text('Correct!');
-		$("#answer-hr").css("visibility", "visible");
-		$("#answers").append(answerP);
-		$("#score-span").text(`Score: ${score}`);
-	} else {
-		score--;
-		wrongAnswer++;
-
-		// no negative scores allowed
-		if (score < 0) {
-			score = 0;
-		}
-
-		answerP.text('Wrong!');
-		$("#answer-hr").css("visibility", "visible");
-		$("#answers").append(answerP);
-		$("#score-span").text(`Score: ${score}`);
-	}
-
-	// displays if the answer is right or wrong for 2 seconds
-	setTimeout(function () {
-		// clears out the hr and the response
-		answerP.text('');
-		$("#answer-hr").css("visibility", "hidden");
-	}, 2000);
-
-	// summon gameover screen and end game if score is 10
-	if (score >= 10) {
-		gameOver();
-	}
-
-	// fetches the next question
-	randomKanjiGame();
-})
-
-function gameOver() {
-	// create and append the restart button
-	var restartButtonEl = $("<button>");
-	restartButtonEl.text("Go Again!");
-	restartButtonEl.attr("id", "restart-button");
-	$("body").append(restartButtonEl);
-
-	// turn off answers
-	$("#answers-base").css("visibility", "hidden");
-
-	//TODO: display summary or results and wrong questions
-}
-
-// restart button event listener
-$("#restart-button").on("click", function () {
-	// reset trackers
-	score = 0;
-	correctAnswer = 0;
-	wrongAnswer = 0;
-
-	// restart game
-	randomKanjiGame();
-
-	// turn on the answers
-	$("#answers-base").css("visibility", "visible");
-
-});
-
-//TODO: Store the answers in an object with their answer and the correct answer
-//TODO: Display at the end their answer and the correct answer saying "You answered X incorrect, here is what you answered, here is the correct answer"
-//TODO: Restart quiz button, clear any cached info about the quiz/answers
-
-
-//TODO: BONUS: Randomly pull a kanji from kanjiapi, pull meaning from that, generate three random words (from an array of words or a dictionary api), append the three answers and the meaning in a random order
-//TODO: BONUS: Click event on the list of answers, check 'this' button against the meaning from the kanjiapi
